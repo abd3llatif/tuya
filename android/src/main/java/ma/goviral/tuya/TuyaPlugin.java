daff01955993969127586509f56a702f1761413e
abd3llatif <>
2022-12-06 12:20:13 +0100

    fix

--- android/src/main/java/ma/goviral/tuya/TuyaPlugin.java
+++ android/src/main/java/ma/goviral/tuya/TuyaPlugin.java
@@ -33,6 +33,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
+import android.content.Context;
+
 
 /** TuyaPlugin */
 public class TuyaPlugin implements FlutterPlugin, MethodCallHandler {
